8868a4644a7501fbdc6c00366ce5d18ab6318774
Mitesh Gohel <meetg7309@gmail.com>
2025-12-08 00:52:26 +0530

    Changes

---
+++ out/wwwroot/Admin/js/form-basic-inputs.js
@@ -0,0 +1,13 @@
+/**
+ * Form Basic Inputs
+ */
+
+'use strict';
+
+(function () {
+  // Indeterminate checkbox
+    const checkbox = document.getElementById('defaultCheck2');
+    if (checkbox != null) {
+        checkbox.indeterminate = true;
+    }
+})();
